545d699810893bc38eb7fcc6decd7d59329b8feb
Dario Quental <dario.quental.000606@edu.atec.pt>
2025-02-20 09:56:47 +0000

    elaboracao do exercicio 1

--- UFCD10789/Exercicio1/Exercicio1.c
+++ UFCD10789/Exercicio1/Exercicio1.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(){
-printf("hello world");
+int Num=0;
+for(int i=0;i<10;i++){
+    Num+=2;
+    printf("Num = %d,i= %d",Num,i);
+    printf("aqui1FOR");
+}
 return 0;
 }
